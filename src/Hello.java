f9a944f2d910a8685b627e29464e84eccb040a5e
tim <zy751724547zy@gmail.com>
2022-04-29 17:12:40 +0800

    dev 333

--- src/Hello.java
+++ src/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("222222");
+        System.out.println("dev 333");
     }
 }
