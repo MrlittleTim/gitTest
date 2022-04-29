b2e4c25109091f02008a30308b85d21e52fe981c
tim <zy751724547zy@gmail.com>
2022-04-29 17:19:31 +0800

    main

--- src/Hello.java
+++ src/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
         System.out.println("hello");
         System.out.println("222222");
         System.out.println("dev 333");
+        System.out.println("main");
     }
 }
